d6104893aeb3d1ad1b9325790e07a486c85c98f4
Jacob Powers <jpowers@23andme.com>
2018-06-14 18:08:20 -0700

    add enzyme tests for account form

--- client/src/components/AccountForm/tests/index.test.js
+++ client/src/components/AccountForm/tests/index.test.js
@@ -1,16 +1,26 @@
 import React from 'react'
 import AccountForm from '../index'
 import renderer from 'react-test-renderer';
+import { mount } from 'enzyme'
+import { Link } from 'react-router-dom'
 import { MemoryRouter } from 'react-router-dom';
 
-test('Renders login page', () => {
+test('Consistently renders the account form', () => {
   const component = renderer.create(
     <MemoryRouter>
       <AccountForm />
     </MemoryRouter>
   )
-
   let tree = component.toJSON()
   expect(tree).toMatchSnapshot()
+})
 
+
+test('Displays the signup Link when enabled', () => {
+  const component = mount(
+    <MemoryRouter>
+      <AccountForm />
+    </MemoryRouter>
+  )
+  expect(component.find(Link).text()).toContain('Signup')
 })
